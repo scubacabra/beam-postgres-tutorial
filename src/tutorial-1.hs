a9d0d140abfc44cf25a074839cd76dc048256e50
Daniel Mijares <daniel.j.mijares@gmail.com>
2017-09-16 15:23:30 -0400

    insertUsers into database.
    This is running insert and run insert from beam-core, not sure why postgres
    backend has same named functions.
    
    sqlite backend doesn't have these functions so that tutorial doesn't need to
    worry about it.

--- src/tutorial-1.hs
+++ src/tutorial-1.hs
@@ -38,6 +38,20 @@ shoppingCartDb = defaultDbSettings
 allUsers :: Q PgSelectSyntax ShoppingCartDb s (UserT (QExpr PgExpressionSyntax s))
 allUsers = all_ (_shoppingCartUsers shoppingCartDb)
 
+insertUsers :: Connection -> IO ()
+insertUsers conn =
+  withDatabaseDebug putStrLn conn $ B.runInsert $
+    B.insert (_shoppingCartUsers shoppingCartDb) $
+    insertValues [ User "james@example.com" "James" "Smith" "b4cc344d25a2efe540adbf2678e2304c" {- james -}
+                 , User "betty@example.com" "Betty" "Jones" "82b054bd83ffad9b6cf8bdb98ce3cc2f" {- betty -}
+                 , User "james@pallo.com" "James" "Pallo" "b4cc344d25a2efe540adbf2678e2304c" {- james -}
+                 , User "betty@sims.com" "Betty" "Sims" "82b054bd83ffad9b6cf8bdb98ce3cc2f" {- betty -}
+                 , User "james@oreily.com" "James" "O'Reily" "b4cc344d25a2efe540adbf2678e2304c" {- james -}
+                 , User "sam@sophitz.com" "Sam" "Sophitz" "332532dcfaa1cbf61e2a266bd723612c" {- sam -}
+                 , User "sam@jely.com" "Sam" "Jely" "332532dcfaa1cbf61e2a266bd723612c" {- sam -}
+                 , User "sam@example.com" "Sam" "Taylor" "332532dcfaa1cbf61e2a266bd723612c" {- sam -}
+                 ]
+
 main :: IO ()
 main = do
   conn <- connectPostgreSQL "dbname=shoppingcart1"
