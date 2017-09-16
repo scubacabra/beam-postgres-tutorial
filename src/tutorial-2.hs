8f33ac904a57901446fe5c1d9fcbb9a4d946eaa4
Daniel Mijares <daniel.j.mijares@gmail.com>
2017-09-16 15:54:01 -0400

    starting tutorial 2

---
+++ src/tutorial-2.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications   #-}
+{-# LANGUAGE TypeFamilies       #-}
+module Tutorial2 where
+
+import           Data.Text                  (Text)
+import           Database.Beam              as B
+import           Database.Beam.Postgres
+import           Database.PostgreSQL.Simple
+
+data UserT f = User
+  { _userEmail     :: Columnar f Text
+  , _userFirstName :: Columnar f Text
+  , _userLastName  :: Columnar f Text
+  , _userPassword  :: Columnar f Text
+  } deriving (Generic)
+
+type User = UserT Identity
+type UserId = PrimaryKey UserT Identity
+
+deriving instance Show User
+
+instance Beamable UserT
+instance Beamable (PrimaryKey UserT)
+
+instance Table UserT where
+  data PrimaryKey UserT f = UserId (Columnar f Text) deriving Generic
+  primaryKey = UserId . _userEmail
+
+data ShoppingCartDb f = ShoppingCartDb
+                      { _shoppingCartUsers :: f (TableEntity UserT) }
+                        deriving Generic
+
+instance Database ShoppingCartDb
+
+shoppingCartDb :: DatabaseSettings be ShoppingCartDb
+shoppingCartDb = defaultDbSettings
+
+allUsers :: Q PgSelectSyntax ShoppingCartDb s (UserT (QExpr PgExpressionSyntax s))
+allUsers = all_ (_shoppingCartUsers shoppingCartDb)
+
+insertUsers :: Connection -> IO ()
+insertUsers conn =
+  withDatabaseDebug putStrLn conn $ B.runInsert $
+    B.insert (_shoppingCartUsers shoppingCartDb) $
+    insertValues [ User "james@example.com" "James" "Smith" "b4cc344d25a2efe540adbf2678e2304c" {- james -}
+                 , User "betty@example.com" "Betty" "Jones" "82b054bd83ffad9b6cf8bdb98ce3cc2f" {- betty -}
+                 , User "james@pallo.com" "James" "Pallo" "b4cc344d25a2efe540adbf2678e2304c" {- james -}
+                 , User "betty@sims.com" "Betty" "Sims" "82b054bd83ffad9b6cf8bdb98ce3cc2f" {- betty -}
+                 , User "james@oreily.com" "James" "O'Reily" "b4cc344d25a2efe540adbf2678e2304c" {- james -}
+                 , User "sam@sophitz.com" "Sam" "Sophitz" "332532dcfaa1cbf61e2a266bd723612c" {- sam -}
+                 , User "sam@jely.com" "Sam" "Jely" "332532dcfaa1cbf61e2a266bd723612c" {- sam -}
+                 , User "sam@example.com" "Sam" "Taylor" "332532dcfaa1cbf61e2a266bd723612c" {- sam -}
+                 ]
+
+selectAllUsers :: Connection -> IO ()
+selectAllUsers conn =
+  withDatabaseDebug putStrLn conn $ do
+    users <- runSelectReturningList $ select allUsers
+    mapM_ (liftIO . putStrLn . show) users
+
+sortUsersByFirstName :: Connection -> IO ()
+sortUsersByFirstName conn =
+  withDatabaseDebug putStrLn conn $ do
+    users <- runSelectReturningList $ select sortUsersByFirstName
+    mapM_ (liftIO . putStrLn . show) users
+  where
+    sortUsersByFirstName = orderBy_ (\u -> (asc_ (_userFirstName u), desc_ (_userLastName u))) allUsers
+
+boundedUsers :: Connection -> IO ()
+boundedUsers conn =
+  withDatabaseDebug putStrLn conn $ do
+    users <- runSelectReturningList $ select boundedQuery
+    mapM_ (liftIO . putStrLn . show) users
+  where
+    boundedQuery = limit_ 1 $ offset_ 1 $ orderBy_ (asc_ . _userFirstName) $ allUsers
+
+userCount :: Connection -> IO ()
+userCount conn =
+  withDatabaseDebug putStrLn conn $ do
+    Just c <- runSelectReturningOne $ select userCount
+    liftIO $ putStrLn ("We have " ++ show c ++ " users in the database")
+  where
+    userCount = aggregate_ (\u -> as_ @Int countAll_) allUsers
+
+numberOfUsersByName :: Connection -> IO ()
+numberOfUsersByName conn =
+  withDatabaseDebug putStrLn conn $ do
+    countedByName <- runSelectReturningList $ select numberOfUsersByName
+    mapM_ (liftIO . putStrLn . show) countedByName
+  where
+    numberOfUsersByName = aggregate_ (\u -> (group_ (_userFirstName u), as_ @Int countAll_)) allUsers
+
+main :: IO ()
+main = do
+  conn <- connectPostgreSQL "dbname=shoppingcart1"
+  return ()
