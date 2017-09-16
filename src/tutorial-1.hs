5c6698eff8540269734025d190670ed022ae96b8
Daniel Mijares <daniel.j.mijares@gmail.com>
2017-09-16 14:50:48 -0400

    defining a user table
    columns in actual db will be
    - email
    - first_name
    - last_name
    - password
    
    also a type synonym to make User look just like a record
    
    also need a type synonym for the user id, that would be the primary key.

--- src/tutorial-1.hs
+++ src/tutorial-1.hs
@@ -1,7 +1,20 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Tutorial1 where
 
+import           Data.Text                  (Text)
+import           Database.Beam              as B
 import           Database.PostgreSQL.Simple
+
+data UserT f = User
+  { _userEmail     :: Columnar f Text
+  , _userFirstName :: Columnar f Text
+  , _userLastName  :: Columnar f Text
+  , _userPassword  :: Columnar f Text
+  } deriving (Generic)
+
+type User = UserT Identity
+type UserId = PrimaryKey UserT Identity
 
 main :: IO ()
 main = do
