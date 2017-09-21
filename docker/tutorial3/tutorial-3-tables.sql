10c20b462551e3a2251e2332b8e1553977aa4d3c
Daniel Mijares <daniel.j.mijares@gmail.com>
2017-09-21 19:21:10 -0400

    docker setup for tutorial 3
    this is following the schema pretty much to a T, adjusting between how sqlite
    declares things as compared to postgres
    
    most notably you lose the autoincrementing in postgres because serial takes care
    of that
    
    again, there is a nullable foreign key on orders at shipping_info__id
    
    not recommended but we will go through the tutorial like this before changing it
    to use the more recommended approach.

---
+++ docker/tutorial3/tutorial-3-tables.sql
@@ -0,0 +1,43 @@
+create table cart_user (
+  email      varchar not null primary key,
+  first_name varchar not null,
+  last_name  varchar not null,
+  password   varchar not null
+);
+
+create table addresses (
+  id              serial primary key,
+  address1        varchar not null,
+  address2        varchar,
+  city            varchar not null,
+  state           varchar not null,
+  zip             varchar not null,
+  for_user__email varchar not null references cart_user (email)
+);
+
+create table products (
+  id          serial primary key,
+  title       varchar not null,
+  description varchar not null,
+  price       integer not null
+);
+
+create table orders (
+  id                  serial primary key,
+  date                timestamp without time zone not null,
+  for_user__email     varchar not null references cart_user (email),
+  ship_to_address__id integer not null references address (id),
+  shipping_info__id   integer /* this is a nullable foreign key as per the tutorial */
+);
+
+create table shipping_info (
+  id              serial primary key,
+  carrier         varchar not null,
+  tracking_number varchar not nulL
+);
+
+create table line_items (
+  item_in_order__id    integer not null references cart_order (id),
+  item_for_product__id integer not null references product (id),
+  item_quantity        integer not nulL
+);
